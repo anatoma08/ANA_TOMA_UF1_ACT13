e25eabadeb512d6dc1f10d3665ab89e9f025adc0
anatoma08 <a23robtomtom@inspedralbes.cat>
2024-11-25 22:35:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ana_toma_uf1_act13/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ana_toma_uf1_act13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) //Cambiar por activity_gird o activity_linear
+
+    }
+}
